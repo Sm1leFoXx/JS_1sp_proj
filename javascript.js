5f37f07756fbf457e67e81311e76f24566b46807
Vladimir Tarasenkov <smilesitess@gmail.com>
2021-11-21 16:35:53 +0300

    finish

--- javascript.js
+++ javascript.js
@@ -7,19 +7,30 @@ const generateRandomNumber = (min, max) => {
 };
 
 const game = () => {
-    const number = generateRandomNumber(1,100);
+    const number = generateRandomNumber(1,10000);
     let correctAnswer;
 
     if (isEven(number)) {
     correctAnswer = 'да';
-    correctAnswer = 'Да';
     } else {
     correctAnswer = 'нет';
     }
     const answer = prompt(`Является ли число ${number} четным?`);
     if (answer === correctAnswer) {
     alert(`Ответ правильный!`);
+    return true;
     } else {
     alert(`Ответ не правильный! Правильный ответ ${correctAnswer}`);
+    return false;
     }
-};
+}
+
+const roundsGame = () => {
+    for (let i = 0; i < 3; i += 1) {
+    const isSuccess = game();
+    if (!isSuccess) {
+        alert('Игра окончена, вы проиграли!')
+        } 
+    }
+    alert('Вы выиграли!!!')
+}
